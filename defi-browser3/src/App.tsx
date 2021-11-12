731fc423cd54a845de7d63b452b5a2e02e7a37a4
rahulmadduluri <rahulmadduluri@gmail.com>
2021-11-11 18:43:11 -0800

    add deposit button

--- defi-browser3/src/App.tsx
+++ defi-browser3/src/App.tsx
@@ -1,10 +1,16 @@
 import ConnectButton from "./components/ConnectButton";
 
 export default function App() {
+
+  function handleDeposit() {
+    // swap stablecoin for curve and then place yearn transaction
+  }
+
   return (
     <div>
       <div>
         <ConnectButton />
+        <button onClick={handleDeposit}>Deposit</button>
       </div>
     </div>
   )
