102f2763dc5b250717f2a35d024658ef21e07948
rahulmadduluri <rahulmadduluri@gmail.com>
2021-11-11 17:01:20 -0800

    use chakra and usedapp libraries

--- defi-browser2/src/components/ConnectButton.tsx
+++ defi-browser2/src/components/ConnectButton.tsx
@@ -1,18 +1,54 @@
-// ConnectButton.tsx
 import { Button, Box, Text } from "@chakra-ui/react";
 import { useEthers, useEtherBalance } from "@usedapp/core";
+import { formatEther } from "@ethersproject/units";
+
 
 export default function ConnectButton() {
-  const {activateBrowserWallet, account } = useEthers();
+  const { activateBrowserWallet, account } = useEthers();
   const etherBalance = useEtherBalance(account);
 
+  function handleConnectWallet() {
+    activateBrowserWallet();
+  }
+
   return account ? (
-    <Box>
-      <Text color="white" fontSize="md">
-        {etherBalance && etherBalance} ETH
-      </Text>
+    <Box
+      display="flex"
+      alignItems="center"
+      background="gray.700"
+      borderRadius="xl"
+      py="0"
+    >
+      <Box px="3">
+        <Text color="white" fontSize="md">
+          {etherBalance && parseFloat(formatEther(etherBalance)).toFixed(3)} ETH
+        </Text>
+      </Box>
+      <Button
+        bg="gray.800"
+        border="1px solid transparent"
+        _hover={{
+          border: "1px",
+          borderStyle: "solid",
+          borderColor: "blue.400",
+          backgroundColor: "gray.700",
+        }}
+        borderRadius="xl"
+        m="1px"
+        px={3}
+        height="38px"
+      >
+        <Text color="white" fontSize="md" fontWeight="medium" mr="2">
+          // check account is defined and then slice the string
+          {account &&
+            `${account.slice(0, 6)}...${account.slice(
+              account.length - 4,
+              account.length
+            )}`}
+        </Text>
+      </Button>
     </Box>
   ) : (
-    <Button>Connect to a wallet</Button>
+    <Button onClick={handleConnectWallet}>Connect to a wallet</Button>
   );
 }
